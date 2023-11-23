d9cf58cd2677f5b8321d219d3db8b7bfcbf8ae6d
Ridlaw Otten <58853278+waldirln@users.noreply.github.com>
2023-11-23 06:30:29 +0100

    Update is_prime.R with tests

--- is_prime.R
+++ is_prime.R
@@ -1,6 +1,15 @@
+# Define function
 is_prime <- function(x) {
 	for (i in seq(2, x - 1)) {
 		if (x %% i == 0) return(FALSE)
 	}
 	return(TRUE)
 }
+
+# Test function
+library("testthat")
+
+test_that("Primes are correctly identified", {
+	# TODO: fix expectations to logical
+	expect_equal(sapply(1:5, is_prime), c(0, 0, 1, 0, 1))
+})
