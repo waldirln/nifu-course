cfca82276e26a533d5a6bcb01b6140fede50101c
Waldir Leoncio <w.l.netto@medisin.uio.no>
2023-11-23 10:41:33 +0100

    Fixed unit tests

--- is_prime.R
+++ is_prime.R
@@ -10,6 +10,8 @@ is_prime <- function(x) {
 library("testthat")
 
 test_that("Primes are correctly identified", {
-	# TODO: fix expectations to logical
-	expect_equal(sapply(1:5, is_prime), c(0, 0, 1, 0, 1))
+	expect_equal(
+	  sapply(1:5, is_prime),
+	  c(FALSE, FALSE, TRUE, FALSE, TRUE)
+  )
 })
