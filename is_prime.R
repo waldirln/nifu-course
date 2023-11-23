470cefd4477e986192a73b37bbca381b9abc7cdc
sda030 <stephus.daus@gmail.com>
2023-11-23 10:42:30 +0100

    La til test

--- is_prime.R
+++ is_prime.R
@@ -13,3 +13,10 @@ test_that("Primes are correctly identified", {
 	# TODO: fix expectations to logical
 	expect_equal(sapply(1:5, is_prime), c(0, 0, 1, 0, 1))
 })
+
+
+test_that("Waldir is a good man", {
+  good <- 42
+  waldir <- 42
+  expect_equal(good, waldir)
+})
