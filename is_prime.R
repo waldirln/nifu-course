693e74d88d6ad4923d29e38b9d22c9860a1465bf
T2deB <145110528+T2deB@users.noreply.github.com>
2023-11-23 10:42:59 +0100

    Update is_prime.R
    Added Obi-Wan Kenobi quote, and deleted some code

--- is_prime.R
+++ is_prime.R
@@ -6,10 +6,5 @@ is_prime <- function(x) {
 	return(TRUE)
 }
 
-# Test function
-library("testthat")
 
-test_that("Primes are correctly identified", {
-	# TODO: fix expectations to logical
-	expect_equal(sapply(1:5, is_prime), c(0, 0, 1, 0, 1))
-})
+My allegiance is to the replubic
